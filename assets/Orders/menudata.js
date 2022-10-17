3053397407308089f4901cd69f948929a98a04ab
vinjent <vinjentvan@hotmail.com>
2022-10-17 16:31:51 +0800

    Add files via upload

---
+++ assets/Orders/menudata.js
@@ -0,0 +1,108 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var menudata={children:[
+{text:"主頁面",url:"index.html"},
+{text:"Packages",url:"namespaces.html",children:[
+{text:"Package List",url:"namespaces.html"},
+{text:"Package 成員列表",url:"namespacemembers.html",children:[
+{text:"全部",url:"namespacemembers.html"},
+{text:"列舉型態",url:"namespacemembers_enum.html"}]}]},
+{text:"類別",url:"annotated.html",children:[
+{text:"複合列表",url:"annotated.html"},
+{text:"複合索引",url:"classes.html"},
+{text:"類別階層",url:"hierarchy.html"},
+{text:"複合成員",url:"functions.html",children:[
+{text:"全部",url:"functions.html",children:[
+{text:"a",url:"functions.html#index_a"},
+{text:"b",url:"functions.html#index_b"},
+{text:"c",url:"functions.html#index_c"},
+{text:"d",url:"functions.html#index_d"},
+{text:"e",url:"functions.html#index_e"},
+{text:"f",url:"functions.html#index_f"},
+{text:"g",url:"functions.html#index_g"},
+{text:"i",url:"functions.html#index_i"},
+{text:"l",url:"functions.html#index_l"},
+{text:"m",url:"functions.html#index_m"},
+{text:"o",url:"functions.html#index_o"},
+{text:"q",url:"functions.html#index_q"},
+{text:"r",url:"functions.html#index_r"},
+{text:"s",url:"functions.html#index_s"},
+{text:"t",url:"functions.html#index_t"},
+{text:"x",url:"functions.html#index_x"},
+{text:"~",url:"functions.html#index__7E"},
+{text:"下",url:"functions.html#index_a0xe4b88b"},
+{text:"交",url:"functions.html#index_a0xe4baa4"},
+{text:"價",url:"functions.html#index_a0xe583b9"},
+{text:"分",url:"functions.html#index_a0xe58886"},
+{text:"回",url:"functions.html#index_a0xe59b9e"},
+{text:"委",url:"functions.html#index_a0xe5a794"},
+{text:"子",url:"functions.html#index_a0xe5ad90"},
+{text:"市",url:"functions.html#index_a0xe5b882"},
+{text:"帳",url:"functions.html#index_a0xe5b8b3"},
+{text:"幣",url:"functions.html#index_a0xe5b9a3"},
+{text:"成",url:"functions.html#index_a0xe68890"},
+{text:"推",url:"functions.html#index_a0xe68ea8"},
+{text:"改",url:"functions.html#index_a0xe694b9"},
+{text:"數",url:"functions.html#index_a0xe695b8"},
+{text:"標",url:"functions.html#index_a0xe6a899"},
+{text:"流",url:"functions.html#index_a0xe6b581"},
+{text:"營",url:"functions.html#index_a0xe7879f"},
+{text:"盤",url:"functions.html#index_a0xe79ba4"},
+{text:"系",url:"functions.html#index_a0xe7b3bb"},
+{text:"網",url:"functions.html#index_a0xe7b6b2"},
+{text:"買",url:"functions.html#index_a0xe8b2b7"},
+{text:"錯",url:"functions.html#index_a0xe98caf"}]},
+{text:"函式",url:"functions_func.html"},
+{text:"變數",url:"functions_vars.html"},
+{text:"屬性(properties)",url:"functions_prop.html",children:[
+{text:"a",url:"functions_prop.html#index_a"},
+{text:"c",url:"functions_prop.html#index_c"},
+{text:"m",url:"functions_prop.html#index_m"},
+{text:"r",url:"functions_prop.html#index_r"},
+{text:"下",url:"functions_prop.html#index_a0xe4b88b"},
+{text:"交",url:"functions_prop.html#index_a0xe4baa4"},
+{text:"價",url:"functions_prop.html#index_a0xe583b9"},
+{text:"分",url:"functions_prop.html#index_a0xe58886"},
+{text:"回",url:"functions_prop.html#index_a0xe59b9e"},
+{text:"委",url:"functions_prop.html#index_a0xe5a794"},
+{text:"子",url:"functions_prop.html#index_a0xe5ad90"},
+{text:"市",url:"functions_prop.html#index_a0xe5b882"},
+{text:"帳",url:"functions_prop.html#index_a0xe5b8b3"},
+{text:"幣",url:"functions_prop.html#index_a0xe5b9a3"},
+{text:"成",url:"functions_prop.html#index_a0xe68890"},
+{text:"推",url:"functions_prop.html#index_a0xe68ea8"},
+{text:"改",url:"functions_prop.html#index_a0xe694b9"},
+{text:"數",url:"functions_prop.html#index_a0xe695b8"},
+{text:"標",url:"functions_prop.html#index_a0xe6a899"},
+{text:"流",url:"functions_prop.html#index_a0xe6b581"},
+{text:"營",url:"functions_prop.html#index_a0xe7879f"},
+{text:"盤",url:"functions_prop.html#index_a0xe79ba4"},
+{text:"系",url:"functions_prop.html#index_a0xe7b3bb"},
+{text:"網",url:"functions_prop.html#index_a0xe7b6b2"},
+{text:"買",url:"functions_prop.html#index_a0xe8b2b7"},
+{text:"錯",url:"functions_prop.html#index_a0xe98caf"}]},
+{text:"Events",url:"functions_evnt.html"}]}]},
+{text:"檔案",url:"files.html",children:[
+{text:"檔案列表",url:"files.html"}]}]}
